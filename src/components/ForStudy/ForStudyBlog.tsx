9abd38204326e6abe89faf64d641578aa4f424f9
puerchik <rumyantsevnn@yandex.ru>
2024-01-31 09:07:18 +0300

    added search for posts via useSearchParams

--- src/components/ForStudy/ForStudyBlog.tsx
+++ src/components/ForStudy/ForStudyBlog.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import s from './ForStudyBlog.module.css';
 
 export type Post = {
@@ -11,6 +11,12 @@ export type Post = {
 
 const ForStudyBlog = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get('post') || '';
+  const latest = searchParams.has('latest');
+
+  console.log(postQuery);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -18,15 +24,31 @@ const ForStudyBlog = () => {
       .then(data => setPosts(data));
   }, []);
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const query = (form.elements.namedItem('search') as HTMLInputElement).value;
+    const checked = (form.elements.namedItem('latest') as HTMLInputElement).value;
+
+    setSearchParams({ post: query });
+  };
+
   return (
     <>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input type="search" name="search" />
+        <input type="checkbox" name="latest" />
+        <input type="submit" value="search" />
+      </form>
       <h1>Our news</h1>
       <ol>
-        {posts.map(post => (
-          <Link key={post.id} to={`/posts/${post.id}`}>
-            <li>{post.title}</li>
-          </Link>
-        ))}
+        {posts
+          .filter(f => f.title.includes(postQuery))
+          .map(post => (
+            <Link key={post.id} to={`/posts/${post.id}`}>
+              <li>{post.title}</li>
+            </Link>
+          ))}
       </ol>
     </>
   );
