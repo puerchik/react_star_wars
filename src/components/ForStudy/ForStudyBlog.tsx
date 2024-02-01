cfa798f97ca54768a11b4fcc49c5b68850ffdf3a
puerchik <rumyantsevnn@yandex.ru>
2024-02-01 11:47:51 +0300

    preloader on ForStudyBlog

--- src/components/ForStudy/ForStudyBlog.tsx
+++ src/components/ForStudy/ForStudyBlog.tsx
@@ -1,6 +1,7 @@
-import { Link, useLoaderData, useSearchParams } from 'react-router-dom';
+import { Await, Link, defer, useLoaderData, useSearchParams } from 'react-router-dom';
 import BlogFilter from './BlogFilter';
 import s from './ForStudyBlog.module.css';
+import { Suspense } from 'react';
 
 export type Post = {
   userId: number;
@@ -10,7 +11,7 @@ export type Post = {
 };
 
 const ForStudyBlog = () => {
-  const posts = useLoaderData() as Post[];
+  const { posts } = useLoaderData() as { posts: Post[] };
   const [searchParams, setSearchParams] = useSearchParams();
 
   const postQuery = searchParams.get('post') || '';
@@ -23,22 +24,36 @@ const ForStudyBlog = () => {
       <BlogFilter postQuery={postQuery} latest={!!latest} setSearchParams={setSearchParams} />
       <h1>Our news</h1>
       <ol>
-        {posts
-          .filter(f => f.title.includes(postQuery) && f.id > startsFrom)
-          .map(post => (
-            <Link key={post.id} to={`/posts/${post.id}`}>
-              <li>{post.title}</li>
-            </Link>
-          ))}
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Await resolve={posts}>
+            {(resolvedPosts: Post[]) => (
+              <>
+                {resolvedPosts
+                  .filter(f => f.title.includes(postQuery) && f.id > startsFrom)
+                  .map(post => (
+                    <Link key={post.id} to={`/posts/${post.id}`}>
+                      <li>{post.title}</li>
+                    </Link>
+                  ))}
+              </>
+            )}
+          </Await>
+        </Suspense>
       </ol>
     </>
   );
 };
 
-export const postsLoader = async () => {
+async function getPosts() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 
   return res.json();
+}
+
+export const postsLoader = async () => {
+  return defer({
+    posts: getPosts(),
+  });
 };
 
 export default ForStudyBlog;
