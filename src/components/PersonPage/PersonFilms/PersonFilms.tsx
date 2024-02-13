e2f58ea86cc1ee9e07e6f64644b30337320017cf
puerchik <rumyantsevnn@yandex.ru>
2024-02-13 11:33:39 +0300

    added styles

--- src/components/PersonPage/PersonFilms/PersonFilms.tsx
+++ src/components/PersonPage/PersonFilms/PersonFilms.tsx
@@ -3,17 +3,19 @@ import s from './PersonFilms.module.css';
 
 const PersonFilms = ({ films }: { films: FilmType[] }) => {
   return (
-    <ul>
-      {films
-        .sort((a, b) => a.episode_id - b.episode_id)
-        .map(film => (
-          <li key={film.episode_id}>
-            <span>Episode {film.episode_id}</span>
-            <span>: </span>
-            <span>{film.title}</span>
-          </li>
-        ))}
-    </ul>
+    <div className={s.wrapper}>
+      <ul className={s.list__container}>
+        {films
+          .sort((a, b) => a.episode_id - b.episode_id)
+          .map(film => (
+            <li className={s.list__item} key={film.episode_id}>
+              <span className={s.item__episode}>Episode {film.episode_id}</span>
+              <span className={s.item__colon}>: </span>
+              <span className={s.item__title}>{film.title}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
