c7e7527867f026a46ba1fce70d48439825b2f971
puerchik <rumyantsevnn@yandex.ru>
2024-02-25 11:09:51 +0300

    added redux persist

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,13 +2,16 @@ import s from './App.module.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../../routes/routesConfig';
 import { Provider } from 'react-redux';
-import { store } from '../../store/store';
+import { persistor, store } from '../../store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <div className={s.wrapper}>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <PersistGate persistor={persistor}>
+          <RouterProvider router={router} />
+        </PersistGate>
       </Provider>
     </div>
   );
