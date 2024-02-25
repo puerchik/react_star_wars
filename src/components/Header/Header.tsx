9524856a44835fc56a9aafe044fdcec07995f7eb
puerchik <rumyantsevnn@yandex.ru>
2024-02-25 11:38:37 +0300

    connected LS with redux persist

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
 import Favorite from '../Favorite';
 import { NavLink } from 'react-router-dom';
-import { SWAPI_PEOPLE_QUERY } from '../../constatnts/api';
+import { SWAPI_PEOPLE_QUERY, SWAPI_ROOT } from '../../constatnts/api';
 import s from './Header.module.css';
+import { useSelector } from 'react-redux';
+import { AppRootStateType } from '../../store/reducers';
+import { NavPage } from '../../utils/network';
 
 const Header = () => {
+  const navigation = useSelector<AppRootStateType, NavPage>(state => state.navigation);
+  const pageNumber =
+    navigation.next === null
+      ? '9'
+      : String(Number(navigation.next?.split(SWAPI_ROOT + SWAPI_PEOPLE_QUERY)[1]) - 1);
+
   return (
     <header className={s.container}>
       <ul className={s.list__container}>
@@ -11,7 +20,7 @@ const Header = () => {
           <NavLink to={'/'}>Home</NavLink>
         </li>
         <li>
-          <NavLink to={`/${SWAPI_PEOPLE_QUERY}${1}`}>People</NavLink>
+          <NavLink to={`/${SWAPI_PEOPLE_QUERY}${pageNumber}`}>People</NavLink>
         </li>
         <li>
           <NavLink to={'/not-found'}>Not Found</NavLink>
