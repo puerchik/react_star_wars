81cbe357dba8b3b8fa99a62d4d8ef571e6a93d81
puerchik <rumyantsevnn@yandex.ru>
2024-03-03 07:58:16 +0300

    connected debounce to search input

--- src/containers/SearchPage/SearchPage.tsx
+++ src/containers/SearchPage/SearchPage.tsx
@@ -1,16 +1,19 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { WithErrorApiProps } from '../PeoplePage/PeoplePage';
 import { ResultPeopleName, getApiResource, Response } from '../../utils/network';
-import WithErrorApi from '../../hoc-helpers/WithErrorApi';
 import { SWAPI_PEOPLE_SEARCH } from '../../constatnts/api';
-import s from './SearchPage.module.css';
+import { debounce } from 'lodash';
+import WithErrorApi from '../../hoc-helpers/WithErrorApi';
 import PeopleList from '../../components/PeoplePage/PeopleList';
+import s from './SearchPage.module.css';
 
 const SearchPage = ({ setError }: WithErrorApiProps) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [peopleList, setPeopleList] = useState<ResultPeopleName[]>([]);
 
   const getResponse = async (param: string) => {
+    console.log(param);
+
     const res: Response = await getApiResource(SWAPI_PEOPLE_SEARCH + param);
     if (res) {
       const people = res.results.map(el => {
@@ -26,11 +29,16 @@ const SearchPage = ({ setError }: WithErrorApiProps) => {
     }
   };
 
+  const debouncedGetResponse = useCallback(
+    debounce(value => getResponse(value), 500),
+    [],
+  );
+
   const handleSearchPeople = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
     setSearchValue(value);
     if (value !== '') {
-      getResponse(value);
+      debouncedGetResponse(value);
     }
   };
 
@@ -40,7 +48,7 @@ const SearchPage = ({ setError }: WithErrorApiProps) => {
       <input
         onChange={handleSearchPeople}
         value={searchValue}
-        type="text"
+        type="search"
         placeholder="Input character's name"
       />
       {peopleList.length === 0 ? <h2>No results</h2> : <PeopleList people={peopleList} />}
