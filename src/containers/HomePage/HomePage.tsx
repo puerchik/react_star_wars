3bb52ee9132b62b0f631d4f088744f70843829a9
puerchik <rumyantsevnn@yandex.ru>
2024-06-16 10:29:55 +0300

    edited styles and adaptive layout

--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ import s from './HomePage.module.css';
 const HomePage = () => {
   return (
     <>
-      <h1 className="header__text">Choose your side</h1>
+      <h1 className={`${s.header__text} header__text`}>Choose your side</h1>
       <ChooseTheme />
     </>
   );
